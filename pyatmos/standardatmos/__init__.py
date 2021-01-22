115558097b6a895fb61d217c75ceb8f6d24c1a57
Chunxiao Li <lcx366@126.com>
2021-01-22 15:30:10 +0800

    comments update

---
+++ pyatmos/standardatmos/__init__.py
@@ -0,0 +1,14 @@
+'''
+U.S. Standard Atmosphere (Range: -0.611 to 1,000 km), 1976
+
+This subpackage defines the following functions:
+
+expo.py
+    expo - Estimate the air densities at given geometric or geopotential altitudes 
+           above the sea level using a exponential atmosphere model.
+ussa76.py
+
+isa - Implements the International Standard Atmosphere(ISA) Model up to 86km. 
+
+lapse_tp - Calculate the temperature and pressure at a given geopotential altitude above base of a specific layer.
+'''              
