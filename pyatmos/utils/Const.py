4d2fb6a31522c88881df9bd042f74a46a848f236
Chunxiao Li <lcx366@126.com>
2021-01-22 15:27:31 +0800

    new script to define some physical constants at sea level

---
+++ pyatmos/utils/Const.py
@@ -0,0 +1,10 @@
+# Some physical constants at sea level
+r0 = 6371.0008 # volumetric radius for the Earth, [km] 
+g0 = 9.80665 # standard gravity acceleration, [m/s^2]
+M0 = 28.9644 # mean molecular weight, [kg/kmol]
+R_air = 8314.32/M0 # gas constant for dry air, [J/kg/K]
+gamma = 1.4 # specific heat ratio, namely the ratio of constant-pressure to constant-volume specific heat
+t0 = 288.15 # temperature, [K]
+p0 = 101325 # pressure, [Pa]
+h0 = 0 # height, [km]   
+   
