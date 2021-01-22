e7fa323f248967668d7785ca1696766daf7cddaa
Chunxiao Li <lcx366@126.com>
2021-01-22 15:33:37 +0800

    comments update

--- pyatmos/__init__.py
+++ pyatmos/__init__.py
@@ -1,9 +1,16 @@
 '''
 pyatmos package
 
-This package is an archive of scientific routines that implements the estimation of atmospheric properties for various atmosphere models.  
+This package is an archive of scientific routines that implements the 
+estimation of atmospheric properties for various atmosphere models, such
+as exponential, coesa76, and nrlmsise00. 
+
+The package mainly estimates density, temperature, pressure and other parameters 
+of air at a set of specific altitudes. For atmosphere below 86 kilometers, it also 
+calculates the speed of sound, viscosity, and thermal conductivity.
 '''    
 
-from .StandardAtmosphere.StandardAtmosphere import isa
+from .standardatmos.expo import expo
+from .standardatmos.coesa76 import coesa76
 from .msise.spaceweather import download_sw,read_sw
 from .msise.nrlmsise00 import nrlmsise00
