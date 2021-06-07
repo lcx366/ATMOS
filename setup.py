707e4ce92144c40c0215f1f6ec9ecc75274a26b7
Chunxiao Li <lcx366@126.com>
2021-06-07 12:53:15 +0800

    update to release v1.2.3

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@ from setuptools import setup,find_packages
 
 setup(
     name = 'pyatmos',
-    version = '1.2.1',
-    description = 'A package to estimate the atmosphere parameters',
+    version = '1.2.3',
+    description = 'A package to estimate the vertical structure of atmosphere with various atmospheric density models',
     author = 'Chunxiao Li',
     author_email = 'lcx366@126.com',
     url = 'https://github.com/lcx366/ATMOS',
     license = 'MIT',
     long_description_content_type = 'text/markdown',
     long_description = open('README.md', 'rb').read().decode('utf-8'),
-    keywords = ['atmosphere models','coesa76','nrlmsise00'],
-    python_requires = '>=3.6',
+    keywords = ['coesa76','nrlmsise00','jb2008'],
+    python_requires = '>=3.8',
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
@@ -28,9 +27,8 @@ setup(
         'scipy',
         'numpy',
         'astropy',
-        ],
-    extras_require={
-        'colored-progress': ['tqdm','colorama'],
-        'legendre': ['pyshtools'],
-        },    
+        'pyshtools',
+        'tqdm',
+        'colorama'
+        ]
     )
