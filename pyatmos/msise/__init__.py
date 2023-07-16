3f6ab6c3c590f149317c40e7da03b083340d0a8f
Chunxiao Li <lcx366@gmail.com>
2023-07-16 23:04:21 +0800

    This file is intentionally empty

--- pyatmos/msise/__init__.py
+++ pyatmos/msise/__init__.py
@@ -1,16 +1,1 @@
-'''
-pyatmos msise subpackage
-
-This subpackage defines the following functions:
-
-nrlmsise00_subfunc.py  - subfunctions that implement the atmospheric model NRLMSISE-00
-
-nrlmsise00.py  - Input interface of NRLMSISE-00
-
-spaceweather.py
-
-    download_sw_nrlmsise00 - Download or update the space weather file for NRLMSISE-00 from www.celestrak.com
-
-    read_sw_nrlmsise00 - Read the space weather file for NRLMSISE-00
-
-'''              
+# This file is intentionally empty
